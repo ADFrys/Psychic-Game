9645d7807918542ae2f7759ce5b88b0b93837be7
AngelaF <ADFrys@users.noreply.github.com>
2017-12-10 21:28:39 -0600

    Fix Javascript wins variable count.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,52 @@
-
-
 var wins = 0;
-
 var losses = 0;
-
 var guessesLeft = 9;
 
-var possible = "abcdefghijklmnopqrstuvwxyz"
+var possible = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var computerKey = function computerChoice() {
-  var computerLetter = possible.charAt(Math.floor(Math.random() * possible.length));
-  return computerLetter;
+var guessSoFarp = document.getElementById("guesses-so-far");
+var guessLeftp = document.getElementById("guesses-left");
+
+var computerKey = possible[Math.floor(Math.random() * possible.length)];
+
+function computerChoice() {
+  computerKey = possible[Math.floor(Math.random() * possible.length)];
  };
-  console.log(computerKey());
+
+var reset = function() {
+  guessesLeft = 9;
+  guessLeftp.textContent = "Guesses left: " + guessesLeft;
+  guessSoFarp.textContent = "Your Guesses so far: ";
+  computerChoice();
+}
 
 document.onkeyup = function(event) {
   var userLetter = String.fromCharCode(event.keyCode).toLowerCase();
-  console.log(userLetter);
 
   if (possible.includes(userLetter)) {
 
+    if (computerKey === userLetter)  {
+	    wins++;
+      var winsP = document.getElementById("wins");
+      winsP.textContent = "Wins: " + wins;
+      reset();
+    }
 
-  if (computerKey() === userLetter){
-	wins++;
-    var winsP = document.getElementById("wins");
-    winsP.textContent = "Wins: " + wins;
-  }
+    else if (guessesLeft>0) {
+	    guessSoFarp.textContent += userLetter + " ";
+	    guessesLeft--;
+	    guessLeftp.textContent = "Guesses left: " + guessesLeft;
+    }
 
-  else if (guessesLeft>0) {
-	var guessSoFarp = document.getElementById("guesses-so-far");
-	guessSoFarp.textContent += userLetter + ", ";
-	guessesLeft--;
-	var guessLeftp = document.getElementById("guesses-left");
-	guessLeftp.textContent = "Guesses left: " + guessesLeft;
+    else {
+  	  losses++;
+  	  var lossP = document.getElementById("losses");
+	    lossP.textContent = "Losses: " + losses;
+      reset();
+    }
   }
 
   else {
-  	losses++;
-  	var lossP = document.getElementById("losses");
-	lossP.textContent = "Losses: " + losses;
-  }
-}
-
-else {
-  alert("You pressed the wrong key!");
-
+    alert("You pressed a wrong key! Please select an 'a' through a 'z' key only");
+  };
 };
-
-
-};
-
-
